9129f0be0f1b92eb57070976152290140b075415
tarek <tarekmahmud4020@gmail.com>
2025-04-19 20:54:53 +0600

    done

--- index.php
+++ index.php
@@ -1,8 +1,161 @@
 <?php include('partials/header.php'); ?>
 
 <main>
-  <h1>Welcome to My Site</h1>
-  <p>This content is dynamic!</p>
+  <!-- hero section -->
+  <section class="hero-bg py-3">
+    <div class="container">
+      <div class="row align-items-center g-4">
+        <div class="col-md-6">
+          <div class="hero-content-wrapper">
+            <h1 class="main-title">For your beauty, we use AI to find the best doctors in the world</h1>
+            <h2 class="sub-title">We accompany and advise you every step of the way!</h2>
+
+            <ul class="hero-list-items list-unstyled">
+              <li><i class="fa-solid fa-circle-check me-2"></i> Only qualified specialists</li>
+              <li> <i class="fa-solid fa-circle-check me-2"></i>Personal care service</li>
+              <li> <i class="fa-solid fa-circle-check me-2"></i>Best prices</li>
+            </ul>
+
+            <div class="mobile-store-img">
+              <img src="" alt="">
+              <img src="" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 ">
+          <div class="right-img">
+            <img src="./assets/images/842250461487814937.png" alt="" class="img-fluid">
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ###hero section -->
+
+  <!-- feature section -->
+  <section>
+    <div class="features-section">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-3 feature-item">
+            <div class="feature-icon"><i class="fa-solid fa-circle-check me-2"></i></div>
+            LATEST AI TECHNOLOGIES
+          </div>
+          <div class="col-md-3 feature-item">
+            <div class="feature-icon"><i class="fa-solid fa-circle-check me-2"></i></div>
+            TRUSTED DOCTORS
+          </div>
+          <div class="col-md-3 feature-item">
+            <div class="feature-icon"><i class="fa-solid fa-circle-check me-2"></i></div>
+            24-HOUR SERVICE
+          </div>
+          <div class="col-md-3 feature-item">
+            <div class="feature-icon"><i class="fa-solid fa-circle-check me-2"></i></div>
+            PRICE GUARANTEE
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- feature section -->
+
+  <!--procedure section -->
+  <section>
+    <div class="procedure-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 procedure-item">
+            <img src="./assets/images/plastische-chirurgie-in-der-turkei-istanbul.jpg" alt="Dental Surgery" class="procedure-image">
+            <div class="procedure-title">DENTAL SURGERY</div>
+          </div>
+          <div class="col-md-3 procedure-item">
+            <img src="./assets/images/haartranplantation-in-der-turkei-istanbul.jpg" alt="Plastic Surgery" class="procedure-image">
+            <div class="procedure-title">PLASTIC SURGERY</div>
+          </div>
+          <div class="col-md-3 procedure-item">
+            <img src="./assets/images/zahnchirurgie-in-der-turkei-istanbul.jpg" alt="Hair Transplantation" class="procedure-image">
+            <div class="procedure-title">HAIR TRANSPLANTATION</div>
+          </div>
+          <div class="col-md-3 procedure-item">
+            <img src="./assets/images/augen-lasern-in-der-turkei-istanbul.jpg" alt="Laser Eye Surgery" class="procedure-image">
+            <div class="procedure-title">LASER EYE SURGERY</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="difference-section">
+      <div class="container">
+        <h2 class="difference-title">The Doctor BeYouti Difference</h2>
+        <p class="difference-text">At Qunomedical, you always come first. Our customer service is available 24/7 and will help you find a high-quality yet affordable option for your treatment. Our team is committed to providing you with access to the highest quality and safest doctors and treatment options—both domestically and internationally.</p>
+      </div>
+    </div>
+  </section>
+  <!--procedure section -->
+
+  <!-- help-section -->
+  <section>
+    <div class="help-section">
+      <div class="container">
+        <h2 class="help-title">How we help you</h2>
+        <div class="row justify-content-center">
+          <div class="col-md-4 help-item">
+            <div class="help-icon">
+              <i class="fa-regular fa-calendar-check"></i>
+            </div>
+            <h3 class="help-item-title">Real patients, real reviews</h3>
+            <p class="help-item-description">Read real reviews and testimonials from real patients who booked through Qunomedical.</p>
+          </div>
+          <div class="col-md-4 help-item">
+            <div class="help-icon">
+              <i class="fa-solid fa-stethoscope"></i>
+            </div>
+            <h3 class="help-item-title">The right doctor and place</h3>
+            <p class="help-item-description">Whether at home or abroad, we provide you with comprehensive information so that you can choose your treatment and the right expert with a clear conscience.</p>
+          </div>
+          <div class="col-md-4 help-item">
+            <div class="help-icon">
+              <i class="fa-regular fa-calendar-check"></i>
+            </div>
+            <h3 class="help-item-title">24-hour customer service</h3>
+            <p class="help-item-description">Our patient managers will assist you with your booking and make your experience as stress-free as possible.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ###help-section -->
+
+  <!-- contact section -->
+  <section>
+    <div class="contact-section">
+      <div class="container">
+        <div class="row align-items-start">
+          <div class="col-md-3 patient-manager-info">
+            <img src="./assets/images/Screenshot-2025-04-19-at-12.00.44 PM-852x1024.png" alt="Emilia" class="patient-manager-image">
+            <p><strong>Your personal<br>patient manager</strong></p>
+            <p>Emilia</p>
+          </div>
+          <div class="col-md-9 contact-details">
+            <h2>Questions? Contact us - We're here for you</h2>
+            <div class="d-flex">
+              <div class="col-lg-8">
+              <p>In a personal consultation, we can discuss your specific concerns and assist you in your decision-making. Let's find the right doctor for you together.</p>
+              </div>
+
+              <div class="btn-wrapper col-lg-4">
+                <a href="tel:+491234242455" class="contact-button">+49 1234242455</a>
+                <a href="mailto:CONTACT@BEYOUTI.DOCTOR" class="contact-button">CONTACT@BEYOUTI.DOCTOR</a>
+                <a href="#" class="contact-button">WHATSAPP</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ###contact section -->
 </main>
 
 <?php include('partials/footer.php'); ?>
--- partials/header.php
+++ partials/header.php
@@ -38,33 +38,52 @@
     <div class="header-bottom">
       <div class="lg-container container-fluid">
         <nav class="navbar navbar-expand-lg">
-          <div class="w-100 d-flex align-item-center">
+          <div class="w-100 d-flex align-item-center justify-content-between">
             <div>
               <a class="navbar-brand" href="#">
                 <img src="../assets/images/Screenshot-2025-04-18-at-8.37.27 PM-1.png" alt="" class="img-fluid">
               </a>
             </div>
             <div class="d-flex align-item-center">
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-            </div>
-            <div class="overflow-hidden">
-              <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">DOCTOR</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">ABOUT US</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">GERMAN</a>
-                  </li>
-                  <li class="nav-item">
-                    <button class="btn btn-light">DOWNLOAD</button>
-                  </li>
-                </ul>
+              <div class="d-flex align-item-center">
+                <button class="navbar-toggler shadow-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+                  <!-- Hamburger Icon -->
+                  <svg class="icon open-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                      d="M1.5 12.5a.5.5 0 0 1 0-1h13a.5.5 0 0 1 0 1h-13zm0-4a.5.5 0 0 1 0-1h13a.5.5 0 0 1 0 1h-13zm0-4a.5.5 0 0 1 0-1h13a.5.5 0 0 1 0 1h-13z" />
+                  </svg>
+
+                  <!-- Close Icon -->
+                  <svg class="icon close-icon d-none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff"
+                    viewBox="0 0 16 16">
+                    <path
+                      d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                  </svg>
+                </button>
+
+
+              </div>
+
+              <div class="d-flex align-item-center">
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">DOCTOR</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">ABOUT US</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">GERMAN</a>
+                    </li>
+                    <li class="nav-item">
+                      <button class="btn btn-light">DOWNLOAD</button>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
